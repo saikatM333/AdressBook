26ddc6678be31ee28a0f6a27e360b78ebccb9180
saikatCV <saikat030703@gmail.com>
2024-04-06 23:39:39 +0530

    added the functionality to add user  on the multiple address bokk and each address book cointains no duplicate entry

--- AddressBook/MultipleAddressBookFunctionality.cs
+++ AddressBook/MultipleAddressBookFunctionality.cs
@@ -9,6 +9,64 @@ namespace AddressBook
     public  class MultipleAddressBookFunctionality
     {
         public static Dictionary<string, Dictionary<string, ContactDetails>> AddressBooks = new Dictionary<string, Dictionary<string, ContactDetails>>();
+        public void addContactToExsistingAddressBook()
+        {
+            Console.WriteLine("enter in which address book you Want to enter the contact details ");
+            string AddressBookName = Console.ReadLine();
+            //AddressBooks["Adress"] = new Dictionary<string, ArrayList> ();
+
+            Console.WriteLine("enter the firstname");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("enter the lastname");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("enter the email");
+            string email = Console.ReadLine();
+            Console.WriteLine("enter the phone");
+            string phone = Console.ReadLine();
+            Console.WriteLine("enter the address");
+            string address = Console.ReadLine();
+            Console.WriteLine("enter the city ");
+            string city = Console.ReadLine();
+            Console.WriteLine("enter the state");
+            string state = Console.ReadLine();
+            Console.WriteLine("enter the zip");
+            string zip = Console.ReadLine();
+
+            // IEnumerable<string> key = AddressBooks[AddressBookName].Keys;
+            if (AddressBooks.ContainsKey(AddressBookName))
+
+            {
+                if (AddressBooks[AddressBookName].ContainsKey($"{firstname} {lastname}"))
+                {
+                    AddressBooks[$"{AddressBookName}"].Add($"{firstname} {lastname}", new ContactDetails(firstname, lastname, email, phone, address, city, state, zip));
+                }
+
+                else
+                {
+                    Console.WriteLine("the user already exist");
+                }
+            }
+
+            else
+            {
+                try
+                {
+                    Console.WriteLine("new address book is maintened");
+                    Dictionary<string, ContactDetails> value = new Dictionary<string, ContactDetails>();
+                    value.Add($"{firstname} {lastname}", new ContactDetails(firstname, lastname, email, phone, address, city, state, zip));
+                    AddressBooks.Add(AddressBookName, value);
+
+                }
+                catch (Exception ex)
+                {
+                    if (ex is CustomException cx)
+                    {
+                        Console.WriteLine(cx);
+                    }
+                }
+            }
+        }
 
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -29,6 +29,27 @@ public class Program
             else
             {
                 Console.WriteLine("this is multiple address book cointaining multiple contact in each address book functionality");
+                try
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("press 0 for add contact details in exsisting or new address book\npress 1 for display of name \n press 2 for count the person by city or state\n press 3 for search by city or state   ");
+                        int choice = Convert.ToInt32(Console.ReadLine());
+
+                        switch (choice)
+                        {
+                            case 0: multipleAddressBookFunctionality.addContactToExsistingAddressBook(); break;
+                            
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex is CustomException cx)
+                    {
+                        Console.WriteLine(cx);
+                    }
+                }
 
             }
         }
